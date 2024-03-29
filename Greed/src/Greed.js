af67ba0ee067ed56d0ce9592d091adda8cceba4c
UnusualSoul <elsonmatthew@yahoo.ca>
2024-03-29 19:15:45 -0400

    fixed the ScoreCalculator function

--- Greed/src/Greed.js
+++ Greed/src/Greed.js
@@ -17,77 +17,48 @@ const pointsTable = {
     5: 50
 }
 
-console.log(ScoreCalculator(1,5,6,4,5)) //200, passed
-console.log(ScoreCalculator(1,5,1,4,5)) //300, passed
-console.log(ScoreCalculator(4,3,6,4,2)) //0, passed
-console.log(ScoreCalculator(1,1,1,4,5)) //1050, passed
-console.log(ScoreCalculator(1,5,6,5,5)) //600, failed
-console.log(ScoreCalculator(1,3,3,3,5)) //450, failed
-
 function ShowScore(score) { 
    const output = document.getElementById("showScore");
 
    output.innerText = score;
 };
 
-//failed due to the three together being between the + 3 index change
-//solution: search by number then increment by the length, not + 3
-//ex: last test above, loop 1 array = 1, loop 2 array = 3,3,3, loop 3 array = 5 
-
-
 function ScoreCalculator(...diceValues){
     let totalPoints = 0;
     diceValues.sort();
     //look through all the dice values and add up the score
-    const valuesArray = []; 
     let keyToMatch = "";
 
-    // let currentDiceSet = 0;
-    // for (let i = 0; i < diceValues.length; i++) {
-    //     currentDiceSet = 10 + diceValues[i];
-    //     //this if (and everything in it) should definitely be a function (will need a couple tweeks though)
-    //     if(i + 1 < diceValues.length) {
-    //         if(diceValues[i + 1] === diceValues[i]) {
-    //             currentDiceSet = valuesArray.at(-1) + 10
-    //             i++;
-    //         }
+    let currentDiceSet = 0;
+    for (let i = 0; i < diceValues.length; i++) {
+        currentDiceSet += diceValues[i];
+        //this if (and everything in it) should definitely be a function (will need a couple tweeks though)
+        if(i + 1 < diceValues.length) {
+            if(diceValues[i + 1] === diceValues[i]) {
+                currentDiceSet *= 11
+                i++;
+            }
 
-    //         if(i + 1 < diceValues.length) {
-    //             if(diceValues[i + 1] === diceValues[i]){
-    //                 currentDiceSet = valuesArray.at(-1) + 10
-    //                 i++;
-    //             }
-    //         }
-    //     }
-    //     valuesArray.push(diceValues[i] + 10)
-    //     currentDiceSet = 0;
-    // }
-    //console.log(valuesArray)
-    do {
-        i += valuesArray.length;
-
-        //after sorting the array we can look at each element and add up the score 
-        valuesArray[0] = diceValues[i];
-        valuesArray[1] = diceValues[i - 1];
-        valuesArray[2] = diceValues[i - 2];
-        //since there may be less diceValues that haven't been checked than 3 we don't want to use those duplicate dice values
-        if(i >= diceValues.length)
-            valuesArray[0] = false;
-        if(i - 1 >= diceValues.length)
-            valuesArray[1] = false;
-        
-        keyToMatch = valuesArray.join("")
+            if(i + 1 < diceValues.length) {
+                if(diceValues[i + 1] === diceValues[i]){
+                    currentDiceSet += 100 * diceValues[i]
+                    i++;
+                }
+            }
+        }
         //search the our points table for a combination of the three values as a string
-        if(Object.keys(pointsTable).includes(keyToMatch)){
-            totalPoints += pointsTable[keyToMatch];
+        if(Object.keys(pointsTable).includes(currentDiceSet.toString())){
+            totalPoints += pointsTable[currentDiceSet];
         }
-        else{
-            let onlyOnes = valuesArray.filter((item) => item === 1)
-            let onlyFives = valuesArray.filter((item) => item === 5)
-            totalPoints += onlyOnes.length * pointsTable["1"] + onlyFives.length * pointsTable["5"];
+        //ugly code, too lazy to fix it
+        else if(Object.keys(pointsTable).includes(currentDiceSet.toString()[0])){
+            totalPoints += pointsTable[currentDiceSet.toString()[0]]
+            if(currentDiceSet.toString().length === 2)
+                totalPoints += pointsTable[currentDiceSet.toString()[1]]
         }
-    } while (i < diceValues.length)
+        //console.log('totalPoints :', totalPoints);
+        currentDiceSet = 0;
+    }
 
-    
     return totalPoints;
 }
